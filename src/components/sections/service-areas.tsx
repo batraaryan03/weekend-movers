46a16f1570d25746d2427aede4d2ce91330dfd95
batraaryan03 <aryanbatra@a2bdigital.org>
2026-06-23 07:49:12 +0530

    chore: remove GlobeDemo component import and usage from service-areas
    Removes the GlobeDemo component and its import from the ServiceAreas section to simplify the page.

--- src/components/sections/service-areas.tsx
+++ src/components/sections/service-areas.tsx
@@ -1,7 +1,5 @@
 "use client";
 
-import { GlobeDemo } from "../ui/globe-demo";
-
 const suburbs = [
   "Brunswick", "Richmond", "Southbank", "Carlton", "St Kilda", "Docklands",
   "Footscray", "Dandenong", "Werribee", "Point Cook", "Craigieburn", "Preston",
@@ -50,9 +48,6 @@ export default function ServiceAreas() {
             No matter where you&apos;re located in Melbourne, Weekend Movers is ready to deliver safe, efficient and professional moving services.
           </p>
         </div>
-      </div>
-      <div className="relative z-10 -mt-8">
-        <GlobeDemo />
       </div>
     </section>
   );
